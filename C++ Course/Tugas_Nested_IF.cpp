b33916a68055556e256df32f6419a40698f282be
Azhar Rizki Zulma <azharrizki@zulma.id>
2024-04-17 18:35:41 +0700

    Create Tugas_Nested_IF.cpp

---
+++ C++ Course/Tugas_Nested_IF.cpp
@@ -0,0 +1,75 @@
+Import Library (menggunakan keyword #include)
+Import Library (menggunakan keyword #include)
+Import Library (menggunakan keyword #include)
+Tambahan Library (menggunakan keyword using)
+
+Deklarasi variabel menu_angka1 (int), menu_angka2 (int)
+Deklarasi variabel angka_1 (int), angka_2 (int), angka_3 (int), hasil (int)
+
+Fungsi Utama (menggunakan keyword int main())
+
+Tampilkan Menu Pertama
+1 Kalkulasi 2 Nilai
+2 Kalkulasi 3 Nilai
+
+Isi Variabel menu_angka1 dengan inputan dari user
+
+Jika menu_angka1 adalah 1, maka:
+
+    Tampilkan Menu Kedua
+    1 adalah Tambah
+    2 adalah Kurang
+    3 adalah Kali
+    4 adalah Bagi
+
+    Isi Variabel menu_angka2 dengan inputan dari user
+    Isi Variabel angka_1 dengan inputan dari user
+    Isi Variabel angka_2 dengan inputan dari user
+
+    Jika menu_angka2 adalah 1, maka:
+        Tambahkan angka_1 dengan angka_2 dan simpan hasilnya ke dalam variabel hasil
+        Tampilkan hasil
+    Selain dari Itu dan Jika menu_angka2 adalah 2, maka:
+        Kurangkan angka_1 dengan angka_2 dan simpan hasilnya ke dalam variabel hasil
+        Tampilkan hasil
+    Selain dari itu dan Jika menu_angka2 adalah 3, maka:
+        Kalikan angka_1 dengan angka_2 dan simpan hasilnya ke dalam variabel hasil
+        Tampilkan hasil
+    Selain dari itu dan Jika menu_angka2 adalah 4, maka:
+        Bagikan angka_1 dengan angka_2 dan simpan hasilnya ke dalam variabel hasil
+        Tampilkan hasil
+    Selain dari itu, maka:
+        Tampilkan "Maaf Menu Tidak Valid"
+
+Selain dari Itu dan Jika menu_angka1 adalah 2, maka:
+
+    Tampilkan Menu Kedua
+    1 adalah Tambah
+    2 adalah Kurang
+    3 adalah Kali
+    4 adalah Bagi
+
+    Isi Variabel menu_angka2 dengan inputan dari user
+    Isi Variabel angka_1 dengan inputan dari user
+    Isi Variabel angka_2 dengan inputan dari user
+    Isi Variabel angka_3 dengan inputan dari user
+
+    Jika menu_angka2 adalah 1, maka:
+        Tambahkan angka_1, angka_2, angka_3 dan simpan hasilnya ke dalam variabel hasil
+        Tampilkan hasil
+    Selain dari Itu dan Jika menu_angka2 adalah 2, maka:
+        Kurangkan angka_1, angka_2, angka_3 dan simpan hasilnya ke dalam variabel hasil
+        Tampilkan hasil
+    Selain dari itu dan Jika menu_angka2 adalah 3, maka:
+        Kalikan angka_1, angka_2, angka_3 dan simpan hasilnya ke dalam variabel hasil
+        Tampilkan hasil
+    Selain dari itu dan Jika menu_angka2 adalah 4, maka:
+        Bagikan angka_1, angka_2, angka_3 dan simpan hasilnya ke dalam variabel hasil
+        Tampilkan hasil
+    Selain dari itu, maka:
+        Tampilkan "Maaf Menu Tidak Valid"
+
+Selain dari Itu, maka:
+    Tampilkan "Maaf Menu Tidak Valid"
+
+Return 0 (menggunakan keyword return 0)
