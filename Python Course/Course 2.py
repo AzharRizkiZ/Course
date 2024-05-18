4f430fc8ad04ed9b8179792a227b767ef7bba924
Azhar Rizki Zulma <azharrizki@zulma.id>
2024-05-18 13:33:54 +0700

    Update Course 2.py

--- Python Course/Course 2.py
+++ Python Course/Course 2.py
@@ -1,1 +1,70 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
 
+# String = Karakter/kata-kata
+# Integer = Bilangan Bulat
+# Float = Bilangan Desimal
+# List = Kumpulan Data
+# Boolean = True/False
+
+"""
+# Fungsi Input
+
+kata = input("Masukkan String: ")
+integer = int(input("Masukkan Angka Integer: "))
+desimal = float(input("Masukkan Angka Desimal: "))
+
+# Fungsi Output
+
+print(kata)
+print(integer)
+print(desimal)
+
+# Constant
+
+INI_CONSTANT = 15
+
+print(INI_CONSTANT)
+
+# Operator Aritmatika adalah operator yang digunakan melakukan operasi matematika
+# Tambah +
+# Kurang -
+# Kali *
+# Bagi /
+# Modulus % (Sisa Hasil Bagi)
+# Pangkat **
+hasil1 = x + y
+hasil2 = x - y
+hasil3 = x * y
+hasil4 = x / y
+hasil5 = x % y #1
+hasil6 = 3**3
+
+# Operator Pembanding digunakan untuk membandingkan dua buah variabel
+Sama Dengan == # is
+Tidak Sama Dengan !=
+Lebih Besar >
+Lebih Kecil <
+Lebih Kecil Sama Dengan <=
+Lebih Besar Sama Dengan >=
+"""
+x = 5
+y = 2
+
+samaDengan = x is not y
+
+# Operator Logika digunakan untuk melakukan pembandingan lebih lanjut pada sebuah program
+operatorAnd = True and True
+operatorOr = True or True
+operatorNegasi = not True # False
+operatorIs = operatorNegasi is False
+
+print(samaDengan)
+
+# Library/Package/Modul Tambahan
+import math
+
+powerMath = math.pow(3, 3)
+squareMath = math.sqrt(81)
+
+print(squareMath)
