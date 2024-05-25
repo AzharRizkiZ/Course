2fbd3b16594683e3dc5d8a499bba18b3f203bc40
Azhar Rizki Zulma <azharrizki@zulma.id>
2024-05-25 15:03:07 +0700

    Create Course 3.py

---
+++ Python Course/Course 3.py
@@ -0,0 +1,19 @@
+Struktur Kendali
+
+Adalah sebuah paradigma untuk mengendalikan alur program ke arah tertentu.
+
+Struktur Percabangan
+
+Strutkur Perulangan
+
+
+Untuk meguasai materi struktur kendali pada pemrograman Python, 
+ada beberapa hal yang diperlukan untuk dipahami
+
+- Ekspresi Boolean
+- Operator
+- - Operator Perbandingan
+- - Operator Logika
+- Konstruksi Program
+
+Nested If dalam Bahasa Python
