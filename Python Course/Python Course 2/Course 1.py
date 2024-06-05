b4b7e728d3e1357230fb2efbaf42c18b1039a085
Azhar Rizki Zulma <azharrizkizulma@gmail.com>
2024-06-05 20:55:46 +0700

    Create Course 1.py

---
+++ Python Course/Python Course 2/Course 1.py
@@ -0,0 +1,18 @@
+# Tipe Data Dalam Bahasa Pemrograman Python
+
+# Integer ==> int = Rounded Numeric
+# Floating Point ==> float = Decimal
+# String ==> str = Merupakan Karakter/Kata/Alphabet/Kalimat
+# String merupakan sebuah list dari banyak karakter
+# Boolean ==> bool = True/False
+# List/Array ==> [] = Daftar
+# List itu selalu diawali dan dimulai dari 0
+
+# Variable
+
+# Variable a quantity that may be changed according to the mathematical problem
+
+str_date = "05-06-2024"
+#           0123456789
+
+print(str_date[:2])
