b0729b852b016b5bbce32911102c96e619565920
Azhar Rizki Zulma <azharrizki@zulma.id>
2024-06-24 21:01:44 +0700

    Update and rename LatihanWhile.py to LatihanWhileFor.py

--- Python Course/LatihanWhileFor.py
+++ Python Course/LatihanWhileFor.py
@@ -1,3 +1,74 @@
+Latihan For Loop
+
+Buatlah sebuah piramida bintang yang ketika diinput jumlah maksimalnya
+maka bintang tersebut akan meyesuaikan dengan jumlah inputan maksimalnya
+
+berikut contohnya
+
+Console:
+Masukkan Jumlah Maksimal Perulangan: 7 (Diinput User)
+7777777
+666666
+55555
+4444
+333
+22
+1
+22
+333
+4444
+55555
+666666
+7777777
+
+
+Clue: For in Range 2 Dimensi
+
+
+maximum = int(input("Masukkan Jumlah Maksimum: "))
+              
+for bawah in range(1, maximum + 1):
+    for samping in range(bawah):
+        print("*", end="")
+    print()
+"""
+## List/Array
+## Kumpulan sebuah daftar data (biasanya setipe, tetapi ada pula yang tidak setipe)
+## Cara mendeklarasikan list menggunakan [] dan setiap data dipisah dengan koma (,)
+
+str_dataBinus = [[1, "Zulma", "1234"],
+                 [2, "Kiraa", "4567"],
+                 [3, "Zavie", "8910"]]
+
+username = input('Masukkan Username: ')
+password = input('Masukkan Password: ')
+
+
+for listKebawah in str_dataBinus:
+    if ( ( listKebawah[1] == username )
+     and ( listKebawah[2] == password ) ) :
+        print("Anda Berhasil Login")
+    else:
+        # Do Nothing
+        print()
+
+i = int(input("masukkan jumlah maksimal: "))
+while ( i > 0 ):
+    j = 0
+    while ( j < i ):
+        print(i, end='')
+        j = j + 1
+    print('')
+    i = i - 1
+
+k = 2
+while ( k <= i ):
+    l = k
+    while ( l > 0 ):
+        print(k, end='')
+        l = l - 1
+    print('')
+    k = k + 1
 
 """
 Buatlah program untuk merata-ratakan 
